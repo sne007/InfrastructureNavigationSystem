891350419c1c6b1cb70d61ff443b1819480b549a
snehith <challasnehith@gmail.com>
2016-11-22 12:49:17 +0530

    Map added to mainActivity, though not sure about efficiency. 2.We can navigate to Map using filters.

--- app/src/main/java/com/example/android/testnavandmaps2/FiltersActivity.java
+++ app/src/main/java/com/example/android/testnavandmaps2/FiltersActivity.java
@@ -1,5 +1,7 @@
 package com.example.android.testnavandmaps2;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
@@ -20,21 +22,50 @@ import java.io.PrintWriter;
  * Created by snehith on 22/11/16.
  */
 
-public class FiltersActivity extends Fragment
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class FiltersActivity extends Fragment {
 
-
+    SendMessage SM;
+    Button button;
+    interface SendMessage{
+        public void sendData(String message);
+    }
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_filters, container, false);
+        View v;
 
+        v =  inflater.inflate(R.layout.fragment_filters, container, false);
+        button = (Button) v.findViewById(R.id.apply_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment fragment = new MapsActivity();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment, fragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+        return v;
     }
-
+/*
+    @Override
+    public void onAttach(Activity activity){
+        super.onAttach(activity);
+    }
 
     @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        return false;
-    }
+    public void onAttach(Context context) {
+        super.onAttach(context);
 
+        try{
+            SM = (SendMessage)context;
+        }
+        catch (ClassCastException e){
+            throw new ClassCastException("You need to implement SendData Methods");
+        }
+
+    }
+    */
 }
